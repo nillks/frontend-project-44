7ad3890afae7a3c83eb3662fdc2e878b252ade2d
nillks <nill.troyanov@gmail.com>
2023-10-13 11:30:38 +0600

    fix: edit error linter

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,7 @@
 import readlineSync from 'readline-sync';
 
-function greetUser() {
+export function greetUser() {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
 }
-
-export default greetUser;
